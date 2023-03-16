6f411a542d719a8adb46922acb53d36b40dd0cc2
Henrique Rieger <henriquerieger2001@gmail.com>
2023-03-16 19:10:07 -0300

    [change] raspa os links de todas as paginas

--- scraper.py
+++ scraper.py
@@ -11,7 +11,19 @@ from selenium.common.exceptions import TimeoutException
 def main():
     driver = webdriver.Chrome()
     driver.get('https://festivaldecuritiba.com.br/atracao/buscar/todas/')
-    plays = get_page_plays(driver)
+
+    plays = []
+    next_btn = get_page_next_btn(driver)
+
+    has_next = True
+    while has_next:
+        plays.extend(get_page_plays(driver))
+        scroll_element_to_center(driver, next_btn)
+        driver.execute_script('arguments[0].click();', next_btn)
+        next_btn = get_page_next_btn(driver)
+        if not next_btn: has_next = False
+    plays.extend(get_page_plays(driver))
+
     for play in plays:
         print(play)
 
@@ -21,14 +33,31 @@ def get_page_plays(driver: webdriver.Remote) -> list:
     links = [get_link(item) for item in items]
     return [Play(link) for link in links]
 
+def get_page_next_btn(driver: webdriver.Remote) -> WebElement:
+    icon = get_element_by_class(driver, 'icon-arrow_carrot-right')
+    if icon:
+        return icon.find_element(By.XPATH, '..')
+    return None
+
 def get_element_by_id(driver: webdriver.Remote, id_name: str, wait_time=10) -> WebElement:
     try:
         wait(driver, wait_time).until(EC.presence_of_element_located((By.ID, id_name)))
     except (NoSuchElementException, TimeoutException) as error:
         print(f"Elemento {id_name} não encontrado")
         return None
-
     return driver.find_element(By.ID, id_name)
+
+def get_element_by_class(driver: webdriver.Remote, class_name: str, wait_time=10) -> WebElement:
+    try:
+        wait(driver, wait_time).until(EC.presence_of_element_located((By.CLASS_NAME, class_name)))
+    except (NoSuchElementException, TimeoutException) as error:
+        print(f"Elemento {class_name} não encontrado")
+        return None
+    return driver.find_element(By.CLASS_NAME, class_name)
+
+def scroll_element_to_center(driver: webdriver.Remote, element: WebElement):
+    scrollElementIntoMiddle = "var viewPortHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);var elementTop = arguments[0].getBoundingClientRect().top;window.scrollBy(0, elementTop-(viewPortHeight/2));"
+    driver.execute_script(scrollElementIntoMiddle, element)
 
 def get_link(item: WebElement) -> str:
     return item.find_element(By.CLASS_NAME, 'attraction-link').get_attribute('href')
